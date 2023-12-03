1b50e93ee20f2997de61e417bb27ad6de23eddd7
Torham <TorhamDev@gmail.com>
2023-12-03 17:16:03 +0330

    Add user already exists exception

--- exceptions.py
+++ exceptions.py
@@ -5,3 +5,9 @@ class UserNotFound(HTTPException):
     def __init__(self) -> None:
         self.status_code = 522
         self.detail = "user not found!"
+
+
+class UserAlreadyExists(HTTPException):
+    def __init__(self) -> None:
+        self.status_code = 400
+        self.detail = "user alreadt exists!"
